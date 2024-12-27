70cc81101278ffccf03a745834f5bcb64c0ad130
KRISHNAprog <golkarkrishna065@gmail.com>
2024-12-27 06:58:03 +0530

    first commit

---
+++ p1.java
@@ -0,0 +1,12 @@
+class launch
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Krishna");
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Hello Krishna");
+	}
+
+}
